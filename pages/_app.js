3780883c82d3d344f65980d72104c38acc05be4a
krishna jasiwal <krishnajaswl@gmail.com>
2025-02-21 11:42:51 +0530

    loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,43 @@
 import Navbar from "@/components/Navbar";
 import { Outfit } from "next/font/google";
 import "@/styles/globals.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Footer from "@/components/Footer";
+import LoadingBar from 'react-top-loading-bar'
+import { useRouter } from "next/router";
 
 // const outfit = Outfit({ subsets: ["latin"], weight: ["400", "500", "600", "700"] });
 
 export default function App({ Component, pageProps }) {
   const [rnum, setRnum] = useState('');
+  const [progress, setProgress] = useState(0)
 
   const buyNow = (itemCode, qty, price, name) => {
     qty = qty - 1;
     // router.push(`/`);
   };
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChangeStart = () => setProgress(40);
+    const handleRouteChangeComplete = () => setProgress(100);
 
+    router.events.on("routeChangeStart", handleRouteChangeStart);
+    router.events.on("routeChangeComplete", handleRouteChangeComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChangeStart);
+      router.events.off("routeChangeComplete", handleRouteChangeComplete);
+    };
+  }, [router]);
   return (
+    <>
+    <LoadingBar
+   color="#1E293B" 
+
+    progress={progress}
+    waitingTime={200}
+    onLoaderFinished={() => setProgress(0)}
+    />
     <div className="app-container">
       <Navbar />
       <main className="main-content">
@@ -22,5 +45,6 @@ export default function App({ Component, pageProps }) {
       </main>
       <Footer />
     </div>
+    </>
   );
 }
