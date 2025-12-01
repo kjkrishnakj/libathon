4a167f807e9fe39cbe8cc7f8c41f17a56e0d0b3a
Krishna Jaiswal <78219949+kjkrishnakj@users.noreply.github.com>
2025-12-01 22:50:52 +0530

    admin problem resolved

--- pages/api/totalBooks.js
+++ pages/api/totalBooks.js
@@ -1,18 +1,18 @@
 import connectDb from "../../middleware/mongoose";
+import Book from "@/models/Book";
 
-import Book from "../../models/Book";
-
-export default async function handler(req, res) {
-  await connectDb();
-
-  if (req.method === "GET") {
-    try {
-      const totalBooks = await Book.countDocuments();
-      res.status(200).json({ totalBooks });
-    } catch (error) {
-      res.status(500).json({ error: "Failed to fetch book count" });
-    }
-  } else {
-    res.status(405).json({ error: "Method not allowed" });
+const handler = async (req, res) => {
+  if (req.method !== "GET") {
+    return res.status(405).json({ error: "Method Not Allowed" });
   }
-}
+
+  try {
+    const totalBooks = await Book.countDocuments();
+    return res.status(200).json({ totalBooks });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Error fetching total books count" });
+  }
+};
+
+export default connectDb(handler);
