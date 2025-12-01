d84d99e37c6f6192e7246362ce5d2df7a4ff956c
Krishna Jaiswal <78219949+kjkrishnakj@users.noreply.github.com>
2025-12-01 22:45:44 +0530

    admin problem resolved

--- pages/api/issuedBooks.js
+++ pages/api/issuedBooks.js
@@ -1,14 +1,18 @@
-
 import Issue from "@/models/Issue";
 import connectDb from "../../middleware/mongoose";
 
-export default async function handler(req, res) {
-  await connectDb();
+const handler = async (req, res) => {
+  if (req.method !== "GET") {
+    return res.status(405).json({ error: "Method Not Allowed" });
+  }
 
   try {
     const issuedBooks = await Issue.countDocuments({ rs: false });
-    res.status(200).json({ issuedBooks });
+    return res.status(200).json({ issuedBooks });
   } catch (error) {
-    res.status(500).json({ error: "Error fetching issued books count" });
+    console.error(error);
+    return res.status(500).json({ error: "Error fetching issued books count" });
   }
-}
+};
+
+export default connectDb(handler);
