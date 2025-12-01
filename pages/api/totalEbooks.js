05fe8dcb3ff3859267dce92bd6960508ec9c7d70
Krishna Jaiswal <78219949+kjkrishnakj@users.noreply.github.com>
2025-12-01 22:46:29 +0530

    admin problem resolved

--- pages/api/totalEbooks.js
+++ pages/api/totalEbooks.js
@@ -1,13 +1,18 @@
 import connectDb from "../../middleware/mongoose";
 import Ebook from "@/models/Ebook";
 
-export default async function handler(req, res) {
-  await connectDb();
+const handler = async (req, res) => {
+  if (req.method !== "GET") {
+    return res.status(405).json({ error: "Method Not Allowed" });
+  }
 
   try {
     const eBooks = await Ebook.countDocuments();
-    res.status(200).json({ eBooks });
+    return res.status(200).json({ eBooks });
   } catch (error) {
-    res.status(500).json({ error: "Error fetching eBooks count" });
+    console.error(error);
+    return res.status(500).json({ error: "Error fetching eBooks count" });
   }
-}
+};
+
+export default connectDb(handler);
